410ed6b072104fca2469566991a4b53865b7f088
Ahmad Idrees <Ahmadidre@outlook.com>
2023-11-19 16:17:35 -0500

    updated

--- app/src/main/java/edu/farmingdale/alrajab/week12_auth_ml_api_demo/LoginActivity.kt
+++ app/src/main/java/edu/farmingdale/alrajab/week12_auth_ml_api_demo/LoginActivity.kt
@@ -3,14 +3,21 @@ package edu.farmingdale.alrajab.week12_auth_ml_api_demo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import edu.farmingdale.alrajab.week12_auth_ml_api_demo.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
-    lateinit var binding:ActivityLoginBinding
+    lateinit var binding: ActivityLoginBinding
     private lateinit var firebaseAuth: FirebaseAuth
+
+    private val RC_SIGN_IN = 9001  // Request code for the Google Sign-In
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,8 +29,13 @@ class LoginActivity : AppCompatActivity() {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
-        binding.loginBtn.setOnClickListener{
-            login();
+
+        binding.loginBtn.setOnClickListener {
+            login()
+        }
+
+        binding.loginBtn2.setOnClickListener {
+            signInWithGoogle()
         }
     }
 
@@ -32,23 +44,59 @@ class LoginActivity : AppCompatActivity() {
         val pass = binding.passET.text.toString()
 
         if (email.isNotEmpty() && pass.isNotEmpty()) {
-
             firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
                 if (it.isSuccessful) {
                     val intent = Intent(this, LandingActivity::class.java)
                     startActivity(intent)
                 } else {
                     Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
-
                 }
             }
         } else {
             Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
-
         }
     }
 
+    private fun signInWithGoogle() {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
 
+        val googleSignInClient = GoogleSignIn.getClient(this, gso)
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
 
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)!!
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                Toast.makeText(this, "Google Sign In failed: ${e.message}", Toast.LENGTH_SHORT).show()
+                Log.d("E1", "Google Sign In failed: ${e.message}")
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        firebaseAuth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = firebaseAuth.currentUser
+                    val intent = Intent(this, LandingActivity::class.java)
+                    startActivity(intent)
+
+                    Toast.makeText(this, "Google Sign In successful!", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Authentication failed: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
 }
